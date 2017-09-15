2ad20f71c58e7620c9ff2805bb3554ffef0622d5
Andrey Semochkin <admin@deepweb.spb.ru>
2017-09-15 16:34:24 +0300

    Update data.go

--- render/data.go
+++ render/data.go
@@ -14,8 +14,8 @@ type Data struct {
 // Render (Data) writes data with custom ContentType.
 func (r Data) Render(w http.ResponseWriter) (err error) {
 	r.WriteContentType(w)
-	_, err = w.Write(r.Data)
-	return
+	w.Write(r.Data)
+	return nil
 }
 
 func (r Data) WriteContentType(w http.ResponseWriter) {
